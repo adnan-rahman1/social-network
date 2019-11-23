e8ed2d7b0bf5d4160daec88863a2b8d4ca10cd92
Adnan Rahman <adnanibnamotin@yahoo.com>
2019-11-22 23:47:53 -0500

    testing file upload system

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const multer = require('multer');
 const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 
@@ -12,6 +13,47 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null, 
+            file.originalname.split('.')[0] + 
+            '-' + 
+            Date.now() + 
+            '.' + 
+            file.originalname.split('.')[1]
+        );
+    }
+});
+function fileFilter (req, file, cb) {
+    const FileType = ["jpg", "png", "jpeg"];
+    const isFileTypeMatched = FileType.some(ext => ext == file.originalname.split(".")[1]);
+    console.log(isFileTypeMatched);
+    if (!isFileTypeMatched) {
+        cb(new Error("Please upload a png file"));
+    }
+    cb(null, true);
+}
+  
+let upload = multer({
+    storage,
+    limits: {
+        fileSize: 90000
+    },
+    fileFilter,
+});
+
+app.post("/upload", upload.single("upload"), (req, res) => {
+    res.send("Image upload successfully");
+})
+
+
 
 
 app.use('/user', userRoute);
